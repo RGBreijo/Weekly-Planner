98166b1003c7d9825a357a585914ffee426765c8
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2020-07-09 10:29:16 -0400

    Added function for addition button

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 
-// Do it first in an id then add it to all the classes with a for loop and get the id name
+
+// Need to do a look that ads an event listener to all the classes then get the ID to add the card. 
+
+let addTaskCardBtn = document.querySelector("#add-mon-card").addEventListener('click', addTaskCard);
+
+
+
+
+
+
+
+ ///////////// ///////////// Creating a task card  ///////////// /////////////
 
 let headingContent = document.createTextNode("HEADING EXAMPLE");
 let taskMainContent = document.createTextNode("Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi amet qui sapiente consequatur, optio adipisci!");
@@ -43,10 +54,68 @@ newTaskCard.appendChild(headingElement);
 newTaskCard.appendChild(textContent);
 newTaskCard.appendChild(newTaskButtonContainer);
 
+///////////// ///////////// ///////////// ///////////// /////////////
+
+
+function createTaskCard() // TODO ADD PARMS FOR HEADING AND P
+{
+    let headingContent = document.createTextNode("HEADING EXAMPLE");
+    let taskMainContent = document.createTextNode("Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi amet qui sapiente consequatur, optio adipisci!");
+    let taskBtnOneContent = document.createTextNode("Edit");
+    let taskBtnTwoContent = document.createTextNode("Remove");
+
+
+    let newTaskCard = document.createElement("div");
+    newTaskCard.classList.add("task");
+
+
+    let headingElement = document.createElement("h1");
+    headingElement.classList.add("task-header");
+    headingElement.appendChild(headingContent);
+
+
+    let textContent = document.createElement("p");
+
+    textContent.classList.add("task-content");
+    textContent.appendChild(taskMainContent);
 
 
 
-//////////////////////////////// testing ////////////////////////////////
+    let newTaskButtonContainer = document.createElement("div");
+    let editBtn = document.createElement("button");
+    let removeBtn = document.createElement("button");
 
-let monday = document.querySelector("#monday");
-monday.appendChild(newTaskCard);
+    newTaskButtonContainer.appendChild(editBtn);
+    newTaskButtonContainer.appendChild(removeBtn);
+
+    newTaskButtonContainer.classList.add("task-actions");
+    newTaskButtonContainer.firstChild.classList.add("edit-btn");
+    newTaskButtonContainer.lastChild.classList.add("remove-btn");
+
+    newTaskButtonContainer.firstChild.appendChild(taskBtnOneContent);
+    newTaskButtonContainer.lastChild.appendChild(taskBtnTwoContent);
+
+
+
+    newTaskCard.appendChild(headingElement);
+    newTaskCard.appendChild(textContent);
+    newTaskCard.appendChild(newTaskButtonContainer);
+
+    return newTaskCard;
+}
+
+
+function addTaskCard()
+{
+    let monday = document.querySelector("#monday");
+    monday.appendChild(createTaskCard());
+}
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////
